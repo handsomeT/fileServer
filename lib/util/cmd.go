5a559c6b3661d5b5c38b6591a84abbb297a9cfb8
siver <2278254874@qq.com>
2019-07-31 16:53:17 +0800

    test

--- lib/util/cmd.go
+++ lib/util/cmd.go
@@ -2,10 +2,12 @@ package util
 
 import (
 	"bytes"
+	"fileServer/lib/log"
 	"os/exec"
 )
 
 func Cmd(data string) ([]byte, error) {
+	log.Debug(data)
 	cmd := exec.Command("/bin/bash", "-c", data)
 	var out bytes.Buffer
 	cmd.Stdout = &out
